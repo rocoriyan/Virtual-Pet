50a76ad85be7886c722b136cdc7da54974ee294a
rocoriyan <rcorrigan.other+github@gmail.com>
2024-01-18 16:40:53 +0000

    fixing classes, started choosePet function

--- script.js
+++ script.js
@@ -1,42 +1,104 @@
 class Animal {
-    constructor(name, species){
+    constructor(name){
         this.name = name;
-        this.species = species;
-        this.meters = {
-            hunger:100, //size of meters
-            thirst:100,
-            boredom:100
-        }
-        this.fill = {
-            hunger:4, //how much each action fills the respective meter
-            thirst:5,
-            boredom:3
-        }
-        this.drain = {
-            hunger:2, //how much the respective meter is drained per length of time
-            thirst:2,
-            boredom:1
-        }
     }
+
+    //variables
+    meters = {
+        hunger:100, //size of meters
+        thirst:100,
+        boredom:100
+    }
+    fill = {
+        hunger:4, //how much each action fills the respective meter
+        thirst:5,
+        boredom:3
+    }
+    drain = {
+        hunger:2, //how much the respective meter is drained per length of time
+        thirst:2,
+        boredom:1
+    }
+    
     fillMeter(meter){
         this.meters(meter)+= this.fill(meter);
     }
     drainMeter(meter){
         this.meters(meter)-= this.drain(meter);
     }
-}
+};
 
-class Animal1 extends Animal{
-    constructor(name){
-        super(name);
-        this.species = "Animal1";
-        this.meters = {
-            newMeterName:80,
-            anotherMeter:120
-        }
+class Duck extends Animal{
+    
+    //variables
+    meters = super.meters;
+    fill = {
+        hunger:4, //how much each action fills the respective meter
+        thirst:5,
+        boredom:3
     }
-    playFetch(){ //action specific to animal1 animal
+    drain = {
+        hunger:2, //how much the respective meter is drained per length of time
+        thirst:2,
+        boredom:1
+    }
+
+    //functions
+    fillMeter(meter){
+        super.fillMeter;
+    }
+    drainMeter(meter){
+        super.drainMeter;
+    }
+    /*quackSong(){ //action specific to duck animal
         this.fillMeter(boredem);
-        //play animation for fetch
+        //play animation for sing
+    }*/
+};
+
+class Frog extends Animal{
+    
+    //variables
+    meters = super.meters;
+    fill = {
+        hunger:5, //how much each action fills the respective meter
+        thirst:3,
+        boredom:4
     }
+    drain = {
+        hunger:2, //how much the respective meter is drained per length of time
+        thirst:1,
+        boredom:2
+    }
+
+    //functions
+    fillMeter(meter){
+        super.fillMeter;
+    }
+    drainMeter(meter){
+        super.drainMeter;
+    }
+    /*skippingRope(){ //action specific to frog animal
+        this.fillMeter(boredem);
+        //play animation for skipping rope
+    }*/
+};
+
+const petSubmit = document.getElementById('petSubmit'); //button that submits pet info in inputs (species & name)
+
+petSubmit.addEventListener('click', submitInfo => {
+});
+
+function choosePet(petChosen, petName){
+    if(petChosen == "Duck"){
+        let animal = new Duck(petName);
+        return animal;
+    }
+    else if(petChosen == "Frog"){
+        let animal = new Frog(petName);
+        return animal;
+    }
+    else{
+        //nothing has been selected
+    };
 }
